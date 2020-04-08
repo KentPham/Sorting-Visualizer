d0daf452f2a26c89b62cf31b82b4bf30daf333c9
Kent Pham <cyprez.pham@gmailcom>
2020-04-07 20:44:32 -0400

    Added functionality to input max sample size

--- src/components/bundles/homepage/Home.js
+++ src/components/bundles/homepage/Home.js
@@ -7,29 +7,36 @@ class Home extends Component {
         super(props);
 
         this.state = {
-            nums: []
+            nums: [],
+            maxSampleSize: 200
         }
+
         this.handleRandomClick = this.handleRandomClick.bind(this);
+        this.handleMaxSampleSizeInput = this.handleMaxSampleSizeInput.bind(this);
     }
 
     handleRandomClick() {
         let rand;
         let tempNums = [];
 
-        for (let i = 0; i < 200; i++) {
+        for (let i = 0; i < this.state.maxSampleSize; i++) {
             rand = Math.round(1 + Math.random() * 100);
             tempNums = [...tempNums, rand]
         }
 
-        this.setState({nums: [...tempNums]})
+        this.setState({nums: [...tempNums]});
     }
+
+    handleMaxSampleSizeInput(e) {
+        this.setState({maxSampleSize: e.target.value});
+    }
+
     render () {
 
-        console.log(this.state.nums);
-        
         return(
             <div>
                 <h1>This is the home page</h1>
+                <input type="number" value={this.state.maxSampleSize} onChange={this.handleMaxSampleSizeInput} />
                 <button onClick={this.handleRandomClick}>Randomize Array</button>
                 <Visualizer array={this.state.nums} />
             </div>
