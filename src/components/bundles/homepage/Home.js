a37d3a62758655e27797dd7e8187814b6d21325e
Kent Pham <cyprez.pham@gmailcom>
2020-05-07 00:45:52 -0400

    SelectSort sorting implemented, need to fix visual

--- src/components/bundles/homepage/Home.js
+++ src/components/bundles/homepage/Home.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import "./Home.css";
 import Visualizer from "../visualizer";
 import { bubblesort } from "../sorts/Bubblesort";
+import { insertionsort } from "../sorts/Insertionsort";
 
 class Home extends Component {
 
     state = {
         nums: [],
         highlighted: [],
+        speed: 10,
         maxSampleSize: 10,
     }
 
@@ -44,7 +46,7 @@ class Home extends Component {
         this.setState({maxSampleSize: input});
     }
 
-    handleTestSort = async () => {
+    handleBubbleSort = async () => {
         let timeline;
         let temp;
         let index = 0;
@@ -78,12 +80,93 @@ class Home extends Component {
         this.zeroHighlight(nums);
     }
 
+    handleSelectSort = async () => {
+        let timeline;
+        let index = 0;
+        let iHolder = -1;
+        let temp;
+        const { nums } = this.state;
+        this.zeroHighlight(nums);
+        let highlighted = [...this.state.highlighted];
+        timeline = insertionsort(nums);
+
+        while (true) {
+            this.zeroHighlight(nums);
+            highlighted = [...this.state.highlighted];
+            if (iHolder !== -1) {
+                highlighted[iHolder] = 2;
+            }
+            if (timeline[index][1] === -1) {
+                if (timeline[index][0] === timeline[index][3]) {
+                    highlighted[timeline[index][0]] = 1;
+                    this.setState({ highlighted: [...highlighted] });
+
+                    await new Promise(resolve => setTimeout(resolve, this.state.speed));
+
+                    highlighted[timeline[index][3]] = 2;
+                    iHolder = timeline[index][3];
+                    this.setState({ highlighted: [...highlighted] });
+                    
+                    await new Promise(resolve => setTimeout(resolve, this.state.speed));
+                } else {
+                    highlighted[timeline[index][0]] = 1;
+                    highlighted[timeline[index][3]] = 2;
+                    iHolder = timeline[index][3];
+                    this.setState({ highlighted: [...highlighted] });
+                    
+                    await new Promise(resolve => setTimeout(resolve, this.state.speed));
+                }
+            } else if (timeline[index][1] === -2) {
+                highlighted[timeline[index][0]] = 2;
+                this.setState({ highlighted: [...highlighted] });
+
+                highlighted[timeline[index][0]] = 1;
+                nums[timeline[index][0]] = timeline[index][3];
+                this.setState({
+                    highlighted: [...highlighted],
+                    nums: [...nums]
+                });
+
+                await new Promise(resolve => setTimeout(resolve, this.state.speed));
+            } else {
+                highlighted[timeline[index][0]] = 1;
+                highlighted[timeline[index][1]] = 1;
+                highlighted[timeline[index][3]] = 2;
+                this.setState({ highlighted: [...highlighted] });
+                
+                await new Promise(resolve => setTimeout(resolve, this.state.speed));
+
+                if (timeline[index][2] === 1) {
+                    temp = nums[timeline[index][0]];
+                    nums[timeline[index][0]] = nums[timeline[index][1]];
+                    nums[timeline[index][1]] = temp
+                    this.setState({ nums: [...nums] });
+
+                    await new Promise(resolve => setTimeout(resolve, this.state.speed));
+                }
+            }
+
+            index++;
+            if (index > timeline.length - 1) {
+                break;
+            }
+        }
+        this.zeroHighlight(nums);
+    }
+
+    handleInsertionSort = async () => {
+
+        // while (true) {
+
+        // }
+    }
+
     render () {
         const { 
             handleRandomClick, 
             handleMaxSampleSizeInput, 
             handleSelectSort, 
-            handleTestSort,
+            handleBubbleSort,
             handleInsertionSort 
         } = this;
 
@@ -111,7 +194,7 @@ class Home extends Component {
                         </button>
                     </div>
                     <div class="col-6 col-flex">
-                        <button class="visualizer-button-format" onClick={handleTestSort}>Bubble</button>
+                        <button class="visualizer-button-format" onClick={handleBubbleSort}>Bubble</button>
                         <button class="visualizer-button-format" onClick={handleSelectSort}>Select</button>
                         <button class="visualizer-button-format" onClick={handleInsertionSort}>Insertion</button>
                     </div>
--- src/components/bundles/sorts/Insertionsort.js
+++ src/components/bundles/sorts/Insertionsort.js
@@ -1,30 +1,38 @@
-export const insertionsort = (nums) => {
+export function insertionsort(unsortedList) {
+    const nums = [...unsortedList];
+    let tempTimeline = [];
     let temp;
     let index = 0;
-
-    console.log(nums);
 
     if (nums[1] < nums[0]) {
         temp = nums[1];
         nums[1] = nums[0];
         nums[0] = temp;
+        tempTimeline.push([0, 1, 1, -1]);
+    } else {
+        tempTimeline.push([0, 1, 0, -1]);
     }
 
     for (let i = 2; i < nums.length; i++) {
+        tempTimeline.push([i, i-1, 0, -1]);
         if (nums[i] < nums[i-1]) {
             for (let j = 0; j < i; j++) {
+                tempTimeline.push([j, -1, 0, index]);
                 if (nums[i] < nums[j]) {
                     index = j;
+                    tempTimeline.push([j, -1, 0, index]);
                     break;
                 }
             }
             temp = nums[i];
             for (let j = i; j > index; j--) {
                 nums[j] = nums[j-1];
+                tempTimeline.push([j, j-1, 1, 0]);
             }
             nums[index] = temp;
+            tempTimeline.push([index, -2, 1, nums[index]]);
         }
     }
 
-    return nums;
+    return tempTimeline;
 }
